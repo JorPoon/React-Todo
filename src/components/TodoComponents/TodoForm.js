bbf74f3c336a6bbfd6af35d6c1ce4a317e0e4227
Jor_Poon <jormpoon@gmail.com>
2019-02-13 16:23:59 -0500

    created Todo Form js component

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const TodoForm = props => {
+    return (
+        <form onSubmit={props.addToDo}>
+            <input>
+                type= "text"
+                value= {props.ToDo}
+                onChange= {props.handleChanges}
+                placeholder= "What do you have to do?"
+            </input>
+            <button type="submit">Add ToDo</button>
+            <button type="submit">Clear</button>
+        </form>
+    );
+};
+
+export default TodoForm;
